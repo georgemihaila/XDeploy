8a65811a0c740f35471d9979e0507621e4751fbd
Mihaila <gmihaila@techteam.com>
2019-11-05 23:42:36 +0200

    Upload multiple files at once

---
+++ XDeploy.Core/Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace XDeploy.Core.Extensions
+{
+    /// <summary>
+    /// Helper methods for lists.
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Splits a list into chunks.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> ChunkBy<T>(this IEnumerable<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+    }
+}
--- XDeploy.Deployer/Infrastructure/Deployment/ApplicationDeployer.cs
+++ XDeploy.Deployer/Infrastructure/Deployment/ApplicationDeployer.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using XDeploy.Core;
 using XDeploy.Core.IO;
+using XDeploy.Core.IO.Extensions;
+using XDeploy.Core.Extensions;
 
 namespace XDeploy.Client.Infrastructure
 {
@@ -27,9 +29,63 @@ namespace XDeploy.Client.Infrastructure
         /// <summary>
         /// Checks for local file changes, compares their versions to the ones on the server and synchronizes them if required.
         /// </summary>
-        public override Task<SynchronizationResult> SynchronizeAsync()
+        public override async Task<SynchronizationResult> SynchronizeAsync()
         {
-            throw new NotImplementedException();
+            SynchronizationResult result = null;
+            
+            var remoteTree = await _api.GetRemoteTreeAsync(_app);
+            _localTree = new Tree(_app.Location);
+            _localTree.Relativize();
+            var diffs = remoteTree.Diff(_localTree, Tree.FileUpdateCheckType.Checksum);
+            var toBeUploaded = diffs
+                .Where(x => x.Type == IODifference.ObjectType.File && (x.DifferenceType == IODifference.IODifferenceType.Addition || x.DifferenceType == IODifference.IODifferenceType.Update))
+                .Select(x => new ExpectedFileInfo()
+            {
+                Filename = x.Path,
+                Checksum = x.Checksum
+            });
+
+            //Ensure removed files and directories are also removed on the server so we don't take up too much of the owner's space. jk, not doing so will (also) fill the deployment computer(s) with no-longer-necessary files.
+            await _api.DoCleanupAsync(_app, diffs);
+            //It doesn't matter whether we have new files to upload
+            if (toBeUploaded.Count() != 0)
+            {
+                result = new SynchronizationResult();
+                LogToConsole("Upload started");
+
+                var jobid = await _api.CreateDeploymentJobAsync(_app, toBeUploaded);
+
+                var chunks = toBeUploaded.ChunkBy(FILES_CHUNK_SIZE); //Do multiple uploads at once instead of one at a time
+                foreach (var chunk in chunks)
+                {
+                    var uploadTasks = chunk.Select(x => Task.Run(async() => 
+                    {
+                        try
+                        {
+                            LogToConsole($"Uploading {x.Filename}...");
+                            var res = await _api.UploadFileIfNotExistsAsync(_app, jobid, x.Filename, x.Checksum, _fileManager.GetFileBytes(x.Filename));
+                            if (res != "Exists")
+                            {
+                                result.NewFiles++;
+                            }
+                        }
+                        catch
+                        {
+                            LogToConsole($"Error uploading file {x.Filename}");
+                        }
+                    }));
+                    await Task.WhenAll(uploadTasks);
+                }
+                await _api.DeleteDeploymentJobAsync(_app, jobid);
+
+                LogToConsole("Upload completed");
+            }
+            else
+            {
+                LogToConsole("No changes");
+            }
+
+            return result;
         }
     }
 }
